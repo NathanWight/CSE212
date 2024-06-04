1aa04fd34eb432e5b120a6c994f9dd5678b5079a
Nathan Wight <NathanWight2@gmail.com>
2024-06-03 19:29:34 -0600

    done

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -8,14 +8,14 @@ public class Node {
     }
 
     public void Insert(int value) {
-        if (value < Data) {
+            if (value < Data) {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
             else
                 Left.Insert(value);
         }
-        else {
+        else if (value > Data) {
             // Insert to the right
             if (Right is null)
                 Right = new Node(value);
@@ -25,12 +25,21 @@ public class Node {
     }
 
     public bool Contains(int value) {
-        // TODO Start Problem 2
+       if (value == Data)
+        return true;
+    else if (value < Data && Left != null)
+        return Left.Contains(value);
+    else if (value > Data && Right != null)
+        return Right.Contains(value);
+    else
         return false;
     }
 
     public int GetHeight() {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        if (this == null)
+        return 0;
+    int leftHeight = Left?.GetHeight() ?? 0;
+    int rightHeight = Right?.GetHeight() ?? 0;
+    return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
